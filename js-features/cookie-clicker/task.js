048e2094a4e9ec18fcb71342224be18041ae82c5
Anastasiya <MelAn_66@mail.ru>
2024-09-24 22:23:06 +0700

    3

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,7 +2,7 @@ const image = document.getElementById("cookie");
 const clicker__counter = document.getElementById("clicker__counter");
 function addText () {
 image.width = 220;
-image.width = 200;
 clicker__counter.textContent ++;
 };
 img.onclick = addText;
+image.width = 200;
