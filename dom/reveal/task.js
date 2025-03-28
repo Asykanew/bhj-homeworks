2a4bac6fb46afa6c2be8a650f07b96a5015325c3
Anastasiya Garaeva <matreshka-23@mail.ru>
2025-03-28 23:14:30 +0700

    the error has been fixed

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,7 +1,7 @@
 const reveals_arr = document.querySelectorAll('.reveal');
 window.addEventListener('scroll', function() {
  
-    for(let i = 0; i++; i < reveals_arr.length) {
+    for(let i = 0; i < reveals_arr.length; i++) {
         revealTop = reveals_arr[i].getBoundingClientRect().top;
         revealBottom = reveals_arr[i].getBoundingClientRect().bottom;
     if(revealTop >= 0 && revealBottom > 0) {
